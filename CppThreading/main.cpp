f99c5dc33e3f540b5543691fef996fc1c47972de
Con Daly <condaly@AppleAir.local>
2017-05-02 03:21:48 +0100

    first

--- CppThreading/main.cpp
+++ CppThreading/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+std::mutex g_i_mutex;
 
-int main(int argc, const char * argv[]) {
+void PrintLoop1(int x)
+{
+    
+    for(int i=0;i<x;i++)
+    {
+        lock_guard<std::mutex> guard(g_i_mutex);
+        cout<<"LoopA "<<i<<endl;
+    }
+}
+
+
+void PrintLoop2(int x)
+{
+    
+    for(int i=0;i<x;i++)
+    {
+        lock_guard<std::mutex> guard(g_i_mutex);
+        cout<<"LoopB "<<i<<endl;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
     // insert code here...
+    thread A(PrintLoop1, 100);
+    thread B(PrintLoop2, 100);
+    
+    A.join();
+    B.join();
     std::cout << "Hello, World!\n";
     return 0;
 }
